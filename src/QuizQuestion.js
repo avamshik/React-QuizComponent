7d43f4f2e54d97933d2adfc8db38007adf1e865c
avamshik <addanki.vamshi@gmail.com>
2018-09-07 18:56:31 +1000

    Displaying An Error Message Based on State
    1. Adding a constructor() to the QuizQuestion class
    2. Setting initial state in the QuizQuestion constructor()
    3. Changing state in QuestQuestion when a button is clicked
    4. Displaying an error message based on values in state

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,6 +3,12 @@ import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
    
+    constructor(props) {
+        super(props);
+
+        this.state = { incorrectAnswer: false };
+    }
+
     render() {
         return (
             <main>
@@ -16,13 +22,17 @@ class QuizQuestion extends Component {
                         })}
                     </ul>
                 </section>
+                { this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null }
             </main>
         )
     }
 
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
+            this.setState({ incorrectAnswer: false });
             this.props.showNextQuestionHandler();
+        } else {
+            this.setState({ incorrectAnswer: true });
         }
     }
 }
